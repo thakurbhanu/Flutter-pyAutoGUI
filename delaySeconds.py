f79f5d04f5389b0edc8a650958b0f77d6f968e1f
Thakur Bhanu Pradeep <58737223+thakurbhanu@users.noreply.github.com>
2020-06-27 23:46:17 +0530

    Added files

---
+++ delaySeconds.py
@@ -0,0 +1,16 @@
+import pyautogui
+
+def twoSecs():
+    pyautogui.sleep(1)
+    pyautogui.press('enter')
+    pyautogui.sleep(2)
+
+def threeSecs():
+    pyautogui.sleep(1)
+    pyautogui.press('enter')
+    pyautogui.sleep(3)
+
+def sixSecs():
+    pyautogui.sleep(1)
+    pyautogui.press('enter')
+    pyautogui.sleep(6)
